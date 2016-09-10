aec724f9fa93e540266236b8e28268396d76b588
Dulio Denis <dulio.denis@yahoo.com>
2016-09-10 13:56:01 -0400

    Initial commit

---
+++ Pledge/Pledge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pledge
+//
+//  Created by Dulio Denis on 9/10/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pledge
-An iOS 10 Swift 3 App that contains an animated login screen.
+# The Pledge
+An iOS 10 / Swift 3 App that contains an animated login screen.
+
+## Licensing
+Pledge is licensed under [the MIT License](LICENSE).
+
+## Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
